94570c71040b496932409cec03ba7938ceb05be6
youssefsoli <youssefdevelops@gmail.com>
2021-01-09 16:32:26 -0700

    Added bodyparser json

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const router = require('./routes/');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
 app.use('/api', router);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
