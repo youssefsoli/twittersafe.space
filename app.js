d9749b5108a74e128383ad301b9573427b6d6e3d
youssefsoli <youssefdevelops@gmail.com>
2021-01-09 15:10:52 -0700

    Added react client

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const router = require('./routes/');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', router);
+app.use('/api', router);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Catch-all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port: ${port}`);
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+      <b>Hello world</b>
+  );
+}
+
+export default App;
