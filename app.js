e7835be71229a14764022270afea2d4635411a92
youssefsoli <youssefdevelops@gmail.com>
2021-01-09 14:51:48 -0700

    Heroku fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes/');
 const app = express();
-const port = process.env.DEVELOPMENT ? 3000 : 80;
+const port = process.env.PORT || 3000;
 
 app.use('/', router);
 
