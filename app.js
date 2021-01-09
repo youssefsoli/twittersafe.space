b2872cd764df1af900f1565ce552c5d9fa98c5e3
youssefsoli <youssefdevelops@gmail.com>
2021-01-09 14:23:28 -0700

    Added routes directory

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const router = require('./routes/');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
