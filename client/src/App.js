a09738523b21e2af56344a679618856073a7539e
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-09 20:35:36 -0700

    Temporary app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { TextField, Grid } from '@material-ui/core';
+import { useState } from 'react';
 
 function App() {
-  return (
-      <div className='App'>
-          <b>Hello world</b>
-      </div>
-  );
+    const [value, setValue] = useState('');
+    return (
+        <div className="App">
+            <Grid container spacing={1} alignItems="center" justify="center">
+                <Grid item>@</Grid>
+                <Grid item>
+                    <TextField
+                        id="search"
+                        label=""
+                        variant="standard"
+                        value={value}
+                        onInput={({ target }) => {
+                            setValue(target.value);
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
