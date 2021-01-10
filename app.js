4cf2b8e337c12d206d7d7987cfda107ce5292996
youssefsoli <youssefdevelops@gmail.com>
2021-01-09 21:08:04 -0700

    Added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const router = require('./routes/');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', router);
 
