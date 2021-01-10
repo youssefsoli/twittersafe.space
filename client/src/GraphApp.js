9a4b3f9175ad5cf13c1e77b50f478c95847b55ac
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-10 10:46:36 -0700

    Changed search button, made circles have pointer cursor, fixed loading bug for open tweets, scaled down loading icon

--- client/src/GraphApp.js
+++ client/src/GraphApp.js
@@ -4,7 +4,14 @@ import {
     Container,
     InputAdornment,
     Button,
+    Paper,
+    InputBase,
+    IconButton,
+    Grid,
+    Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import { useState } from 'react';
 import { Tweet } from 'react-twitter-widgets';
@@ -21,6 +28,26 @@ const getTweets = async (username) => {
         });
 };
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 28,
+        margin: 4,
+    },
+}));
+
 function App() {
     const [username, setUsername] = useState('');
     const [tweetData, setTweetData] = useState(false);
@@ -28,6 +55,7 @@ function App() {
     const [tweetLoading, setTweetLoading] = useState(false);
     const [error, setError] = useState(false);
     const [tweetID, setTweetID] = useState(false);
+    const classes = useStyles();
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -41,7 +69,12 @@ function App() {
         <div className="App">
             <Container justifyitems="center" alignitems="center">
                 <div>
-                    <img src="logo.svg" width="10%" alt="logo" />
+                    <img
+                        src="logo.svg"
+                        width="10%"
+                        alt="logo"
+                        className="logo"
+                    />
                 </div>
                 <div>
                     <TextField
@@ -51,6 +84,8 @@ function App() {
                         value={username}
                         onKeyPress={async (ev) => {
                             if (ev.key === 'Enter') {
+                                setTweetID(false);
+                                setTweetData(false);
                                 setLoading(true);
                                 let tweetData = false;
                                 try {
@@ -73,34 +108,43 @@ function App() {
                                     @
                                 </InputAdornment>
                             ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        type="submit"
+                                        className={classes.iconButton}
+                                        aria-label="search"
+                                        onClick={async () => {
+                                            setTweetID(false);
+                                            setTweetData(false);
+                                            setLoading(true);
+                                            let tweetData = false;
+                                            try {
+                                                tweetData = await getTweets(
+                                                    username
+                                                );
+                                                setError(false);
+                                            } catch (e) {
+                                                setError(e);
+                                            }
+                                            setLoading(false);
+                                            setTweetData(tweetData || false);
+                                        }}
+                                    >
+                                        <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
                         }}
                     />
                 </div>
-                <div>
-                    <Button
-                        variant="contained"
-                        onClick={async () => {
-                            setLoading(true);
-                            let tweetData = false;
-                            try {
-                                tweetData = await getTweets(username);
-                                setError(false);
-                            } catch (e) {
-                                setError(e);
-                            }
-                            setLoading(false);
-                            setTweetData(tweetData || false);
-                        }}
-                    >
-                        fetch
-                    </Button>
-                </div>
+
                 {loading && (
                     <div>
                         <Lottie
                             options={defaultOptions}
-                            height={200}
-                            width={200}
+                            height={150}
+                            width={150}
                         />
                     </div>
                 )}
@@ -113,11 +157,30 @@ function App() {
                 )}
 
                 {tweetData && (
-                    <Graph
-                        tweetData={tweetData}
-                        setTweetID={setTweetID}
-                        setTweetLoading={setTweetLoading}
-                    />
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-evenly"
+                        alignItems="flex-start"
+                        spacing={3}
+                    >
+                        <Grid item xs={2}>
+                            <Typography align="left">Negative</Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Typography align="center">Neutral</Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Typography align="right">Positive</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Graph
+                                tweetData={tweetData}
+                                setTweetID={setTweetID}
+                                setTweetLoading={setTweetLoading}
+                            />
+                        </Grid>
+                    </Grid>
                 )}
 
                 {tweetLoading && (
