e46010e702e93ecede1c3ad9c7bd5c4159745f59
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-09 18:07:35 -0700

    Added sentiment analysis to GET tweet API

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,5 +1,7 @@
-const Twitter = require('twitter-v2');
 const express = require('express');
+const Twitter = require('twitter-v2');
+const language = require('@google-cloud/language');
+const langClient = new language.LanguageServiceClient();
 
 const router = express.Router();
 
@@ -27,12 +29,48 @@ const getTweets = async (username, client) => {
     return tweets;
 };
 
+const sanitizeTweet = (text) => {
+    return text
+        .replace(/(?:https?|ftp):\/\/[\n\S]+/g, '') // remove any urls
+        .replace(/\n/g, ' ')
+        .replace(/&amp;/g, '')
+        .replace(/\s\s+/g, ' ');
+};
+
+const addSentiments = (tweetsData) => {
+    return Promise.all(
+        tweetsData.map(async (tweet) => {
+            const text = sanitizeTweet(tweet.text);
+            const doc = {
+                content: text,
+                type: 'PLAIN_TEXT',
+            };
+            const [result] = await langClient.analyzeSentiment({
+                document: doc,
+            });
+            return Promise.resolve({
+                text: text,
+                score:
+                    result.documentSentiment.score *
+                    result.documentSentiment.magnitude,
+            });
+        })
+    );
+};
+
 router.get('/:id', (req, res) => {
     const client = getTwitterClient();
     // TODO: account for more than 800 tweets
     getTweets(req.params.id, client)
         .then((tweets) => {
-            res.send(tweets);
+            addSentiments(tweets.data).then((calculatedTweets) => {
+                const averageScore =
+                    calculatedTweets.reduce(
+                        (acc, curr) => acc + curr.score,
+                        0
+                    ) / calculatedTweets.length;
+                res.send({ calculatedTweets, averageScore });
+            });
         })
         .catch((err) => console.log(err));
 });
